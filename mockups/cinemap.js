ef93fbfad7fa010c743b1b949a78248e78edacad
Ekkart Kleinod <ekkart.kleinod@fokus.fraunhofer.de>
2015-03-13 17:35:05 +0100

    a mockup

---
+++ mockups/cinemap.js
@@ -0,0 +1,28 @@
+var map = L.map('map').setView([52.5133, 13.40332], 11);
+
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	id: 'examples.map-i875mjb7'
+}).addTo(map);
+
+
+L.marker([52.5136, 13.39276]).addTo(map)
+	.bindPopup("<b>V for Vendetta</b><br />More information here.");
+
+
+
+var popup = L.popup();
+
+function onMapClick(e) {
+	popup
+		.setLatLng(e.latlng)
+		.setContent("You clicked the map at " + e.latlng.toString())
+		.openOn(map);
+}
+
+map.on('click', onMapClick);
+
+
