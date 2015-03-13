37aa96f4fffed0b21188ebd7bc5d89fca89b3b7e
Ekkart Kleinod <ekleinod@edgesoft.de>
2015-03-14 00:30:04 +0100

    some data for tests

--- mockups/cinemap.js
+++ mockups/cinemap.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([52.5133, 13.40332], 11);
+var map = L.map('map').setView([52.5162, 13.3778], 11);
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 	maxZoom: 18,
@@ -8,11 +8,25 @@ L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 	id: 'examples.map-i875mjb7'
 }).addTo(map);
 
+// marker start
 
-L.marker([52.5136, 13.39276]).addTo(map)
-	.bindPopup("<b>V for Vendetta</b><br />More information here.");
+// Æon Flux
+L.marker([52.5190, 13.3647]).addTo(map)
+	.bindPopup('<b>Æon Flux</b><br />Headquarter (Entry).<br /><a href="#">Image</a><br /><a href="#">Details</a>');
+L.marker([52.4315, 13.5303]).addTo(map)
+	.bindPopup('<b>Æon Flux</b><br />Headquarter (Hallway).<br /><a href="#">Image</a><br /><a href="#">Details</a>');
+L.marker([52.5751, 13.5455]).addTo(map)
+	.bindPopup('<b>Æon Flux</b><br />Headquarter (Inner court).<br /><a href="#">Image</a><br /><a href="#">Details</a>');
 
+// Around the World in 80 Days
+L.marker([52.5136, 13.3928]).addTo(map)
+	.bindPopup('<b>Around the World in 80 Days</b><br />Some place.<br /><a href="#">Image</a><br /><a href="#">Details</a>');
 
+// V for Vendetta
+L.marker([52.5136, 13.3922]).addTo(map)
+	.bindPopup('<b>V for Vendetta</b><br />Palace.<br /><a href="#">Image</a><br /><a href="#">Details</a>');
+
+// marker end
 
 var popup = L.popup();
 
